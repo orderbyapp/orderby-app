fa9be9b6a7690f892c1aaeb814bc0458538af147
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-03-25 21:30:07 +0100

    feat: top menu done and action in shopping cart completed

--- src/App.js
+++ src/App.js
@@ -12,44 +12,16 @@ import OrderLunch from './components/OrderLunch/OrderLunch';
 
 
 class App extends Component {
-  state = {
-    order : {
-      open : false
-    }
-  }
 
-  orderVisibility = () => {
-    this.setState({
-      order : {
-        ...this.order,
-        open: this.state.order.open ? false : true}
-    }, () => console.log(this.state.order.open))
-  }
 
   render() {
     return (
       <div className="App">
-        {/* <Slidemenu open={this.state.order.open} >
-          <Fragment>
-            <OrderLunch>
-            {this.state.order.open && 
-              <button onClick={this.orderVisibility}>
-                <i class="fa fa-chevron-up"></i>
-              </button> 
-              }
-              
-            </OrderLunch>
-           
-          </Fragment>
-        </Slidemenu>
-        <button onClick={this.orderVisibility}>
-                <i class="fa fa-chevron-down"></i>
-              </button>  */}
         {/* <Intro></Intro> */}
         {/* <Loader></Loader> */}
         {/* <Button color='btn btn-order-by' width='w-100' border='border-pink' text='Ver carta'></Button> */}
-         <Board></Board> 
-         {/* <OrderBoard></OrderBoard> */}
+         {/* <Board></Board>  */}
+         <OrderBoard></OrderBoard>
       </div>
     );
   }
--- src/components/OrderBoard/MenuTop.js
+++ src/components/OrderBoard/MenuTop.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './MenuTop.css'
+import Slidemenu from '../Slidemenu/Slidemenu'
+import OrderLunch from '../OrderLunch/OrderLunch'
 
 class MenuTop extends Component {
+  state = {
+    order : {
+      open : false
+    }
+  }
+
+  orderVisibility = () => {
+    this.setState({
+      order : {
+        ...this.order,
+        open: this.state.order.open ? false : true}
+    }, () => console.log(this.state.order.open))
+  }
   render() {
     return (
+      <div>  
       <div className='container-menu margin-top-16'>
         <div className='container padding-menu-nav'>
           <nav className='top-menu'>
-            <span class="material-icons white">arrow_back</span>
-            <span class="material-icons white">restaurant_menu</span>
+            <span className="material-icons white">arrow_back</span>
+            <span className="material-icons white"><a onClick={this.orderVisibility}>restaurant_menu</a></span>
           </nav>
         </div>
+      </div>
+       <Slidemenu open={this.state.order.open} >
+          <Fragment>
+            <OrderLunch>
+            {this.state.order.open && 
+              <button onClick={this.orderVisibility}>
+                <i class="fa fa-chevron-up"></i>
+              </button> 
+              }
+            </OrderLunch>
+          </Fragment>
+        </Slidemenu>
       </div>
     );
   }
