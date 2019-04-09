f9614748804e924bb8255b85c342d8e8b9ceede9
Moises Garcia <moisesgarcia83@gmail.com>
2019-04-09 22:15:42 +0200

    feat:docs folder

---
+++ docs/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-messaging.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+
+
+
+firebase.initializeApp({messagingSenderId : '401884005336'});
+const messaging = firebase.messaging();
+// self.addEventListener('notificationclick', function(event) {
+//   console.log('[Service Worker] Notification click Received.');
+//   event.notification.close();
+//   event.waitUntil(
+//     clients.openWindow('http://localhost:3000/#/messages/')
+//   );
+// });
