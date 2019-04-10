211be2ea9d7e07deb287d4f227bf68d1fc9504f7
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-04-10 20:07:28 +0200

    feat: table detail by-doing

--- src/components/Waiter/TableDetail.js
+++ src/components/Waiter/TableDetail.js
@@ -32,53 +32,53 @@ class TableDetail extends Component {
     return (
       <div className='container mt-5'>
       <div id="accordion">
-      <div class="card">
-        <div class="card-header d-flex" id="headingOne">
-        <i class="material-icons pink">info</i> 
+      <div className="card">
+        <div className="card-header d-flex" id="headingOne">
+        <i className="material-icons pink">info</i> 
 
-          <h5 class="mb-0">
-            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <h5 className="mb-0">
+            <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
             Información General
             </button>
           </h5>
         </div>
     
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-          <div class="card-body">
+        <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div className="card-body">
             <h5>{table.diners} Comensales</h5>
             <hr></hr>
             <h5>Estado de pedido: {order.kitchenStatus}</h5>
             <hr></hr>
-            <h5><i class="fas fa-circle"></i> Estado de mesa: {table.state}</h5>
-            <hr></hr>
-
+            <h5><i className="fa fa-circle pink"></i> {table.state}</h5>
            </div>
         </div>
       </div>
-      <div class="card">
-        <div class="card-header" id="headingTwo">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Collapsible Group Item #2
+      <div className="card">
+        <div className="card-header" id="headingTwo">
+        <i className="material-icons pink">euro_symbol</i>
+
+          <h5 className="mb-0">
+            <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Cuenta actual
             </button>
           </h5>
         </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div className="card-body">
+          {order.products.map(e =>{ return <h2 key={e.title}>{e.title}</h2>} )}
           </div>
         </div>
       </div>
-      <div class="card">
-        <div class="card-header" id="headingThree">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      <div className="card">
+        <div className="card-header" id="headingThree">
+          <h5 className="mb-0">
+            <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
               Collapsible Group Item #3
             </button>
           </h5>
         </div>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-          <div class="card-body">
+        <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+          <div className="card-body">
             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
           </div>
         </div>
