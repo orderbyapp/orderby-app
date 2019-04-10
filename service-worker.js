b9b212b9136e8b84a789deae6e5816485e9188a6
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-04-10 11:31:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/orderby-app/precache-manifest.651bde63f94a9a7ea1afb75756bd3bfb.js"
+  "/orderby-app/precache-manifest.60c623c95f172687bd9e41b3a3135289.js"
 );
 
 workbox.clientsClaim();
