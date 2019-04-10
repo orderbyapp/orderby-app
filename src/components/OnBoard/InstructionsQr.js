a53b5b1459956ba463f2f1356557087b09108c13
Moises Garcia <moisesgarcia83@gmail.com>
2019-04-10 21:28:29 +0200

    feat: test component qr

--- src/components/OnBoard/InstructionsQr.js
+++ src/components/OnBoard/InstructionsQr.js
@@ -1,56 +1,51 @@
 import React, { Component } from 'react'
 import QrReader from 'react-qr-reader'
 
-export default class Instructions extends Component {
-
+class Instructions extends Component {
   constructor(props){
     super(props)
     this.state = {
-        delay: 100,
-        result: 'No result',
+      delay: 100,
+      result: 'No result',
     }
 
     this.handleScan = this.handleScan.bind(this)
     this.openImageDialog = this.openImageDialog.bind(this)
-}
-handleScan(data){
-    this.setState({
-        result: data,
-    })
-}
-handleError(err){
+  }
+  handleScan(result){
+    if(result){
+      this.setState({ result })
+    }
+  }
+  handleError(err){
     console.error(err)
-}
-openImageDialog() {
+  }
+  openImageDialog() {
     this.refs.qrReader1.openImageDialog()
-}
-  render() {
+  }
+
+  render(){
     const previewStyle = {
       height: 240,
       width: 320,
-  }
-    return (
-      <div className='' >
-              <div className=''>
-                <h6 className='mt-4'>Escanea el código QR que hay en la mesa</h6>
-              </div>
-              <div>
-                <QrReader ref="qrReader1"
-                    delay={300}
-                    previewStyle={previewStyle}
-                    onError={this.handleError}
-                    onScan={this.handleScan}
-                    legacyMode={true} 
-                />
-                <p>{this.state.result}</p>
-                <input type="button" value="Submit QR Code" onClick={this.openImageDialog} />
-            </div>
+    }
+
+    return(
+      <div>
+        <QrReader
+          ref="qrReader1"
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          legacyMode
+        />
+        <input type="button" value="Submit QR Code" onClick={this.openImageDialog} />
+        <p>{this.state.result}</p>
       </div>
     )
   }
 }
- 
 
-
-
+export default Instructions
 
