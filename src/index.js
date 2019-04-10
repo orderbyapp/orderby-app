a0dfaf7ac3d1ac48bef00ffd2cf91d35689ed8e7
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-04-10 19:06:41 +0200

    feat: pushed and performed waiters page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/collapse'
+import 'jquery'
 import 'font-awesome/css/font-awesome.min.css';
 import 'material-icons/iconfont/material-icons.css'
 //import { initializeFirebase, askForPermissioToReceiveNotifications } from './push-notification';
