a64312e48b916c9399d6cff6658ea8fe45719aaf
Moises Garcia <moisesgarcia83@gmail.com>
2019-04-10 16:27:25 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/orderby-app/precache-manifest.5066fffe357afb3b85a2a72768fed80e.js"
+  "/orderby-app/precache-manifest.f947ff0bd0f8cbe2d705e7f5d4d15933.js"
 );
 
 workbox.clientsClaim();
