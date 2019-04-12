3adcfb533668b7bfce519ebb4dbec10384aa522b
Moises Garcia <moisesgarcia83@gmail.com>
2019-04-12 10:21:53 +0200

    feat: animatedSwitch changes

--- src/App.js
+++ src/App.js
@@ -20,6 +20,41 @@ import TableDetail from './components/Waiter/TableDetail';
 import WaiterIntro from './components/Waiter/IntroWaiter';
 
 
+function mapStyles(styles) {
+    return {
+      opacity: styles.opacity,
+      transform: `scale(${styles.scale})`,
+    };
+  }
+  
+  // wrap the `spring` helper to use a bouncy config
+  function bounce(val) {
+    return spring(val, {
+      stiffness: 180,
+      damping: 22,
+    });
+  }
+  
+  // child matches will...
+  const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+      opacity: 0,
+      scale: 0.5,
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+      opacity: bounce(0),
+      scale: bounce(0.5),
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+      opacity: bounce(1),
+      scale: bounce(1),
+    },
+  };
+  
+
 class App extends Component {
     render() {
         return (
@@ -33,11 +68,11 @@ class App extends Component {
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/waiter" component={WaiterBoard}/>
                     <AnimatedSwitch
-                        atEnter={{ opacity: 0 }}
-                        atLeave={{ opacity: 0 }}
-                        atActive={{ opacity: 1 }}
-                        className="switch-wrapper">
-                        
+                        atEnter={bounceTransition.atEnter}
+                        atLeave={bounceTransition.atLeave}
+                        atActive={bounceTransition.atActive}
+                        mapStyles={mapStyles}
+                        className="route-wrapper">
                         <Route exact path="/waiterIntro" component={WaiterIntro}/>
                         <Route exact path='/waiter/:id' component={TableDetail}/>
                         <TableRoute exact path='/board' component={Board}/>
