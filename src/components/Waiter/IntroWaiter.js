bf5b8c7c6eaa63ea6bfa9254b301184933e1bd5b
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-04-12 19:10:39 +0200

    feat: intro redirect to login

--- src/components/Waiter/IntroWaiter.js
+++ src/components/Waiter/IntroWaiter.js
@@ -9,24 +9,44 @@ import yellow from '../../sources/path-6.svg'
 import blue from '../../sources/oval.svg'
 import text from '../../sources/group-32.png'
 import './Intro.css'
+import {Redirect} from 'react-router-dom'
+
 
 // Hay que crear un observable donde venga: Restaurante, Mesa, Menu y Camarero
 // Despues que se haya creado el observable, hay que hacer un set-time-out y que redirija a las promociones, al primer board.
 
 class WaiterIntro extends Component {
+    state = {
+        timeout: false
+    }
+
+    redirectLogin = () => {
+        setTimeout(() => {
+            this.setState({
+              timeout: true
+            })
+           }, 3000)  
+    }
+
     render() {
-        return (
-            <React.Fragment>
-                <div className="intro-bg overflow-x-hidden">
+         if(!this.state.timeout){
+            return (
+            <React.Fragment activeRedirect={this.redirectLogin()}>
+                <div className="intro-bg overflow-x-hidden" >
                   <img alt='path-orange' src={orange} className="path-7 scale-in-bl"/>
                   <img alt='doggy-intro' src={doggo} className="doggo slide-in-blurred-bl"/>
                   <img alt='boy-intro' src={boy} className="boy slide-in-blurred-bl"/>
                   <img alt='yellow-intro' src={yellow} className="path-6 scale-in-tr"/>
                   <img alt='blue-intro' src={blue} className="path-3 scale-in-left "/>
-                  <img alt='text-intro-waiter' src={text} className="text-intro-logo flip-in-diag-2-br"/>
+                  <img alt='text-intro-waiter' src={text} className="text-intro-waiter flip-in-diag-2-br"/>
                 </div>
             </React.Fragment>
         );
+         }else {
+                return (
+                    <Redirect to='/login'></Redirect>
+                )
+            }
     }
 }
 
