def963757715d8500a766a3d13b95c9e85fafdda
Moises Garcia <moisesgarcia83@gmail.com>
2019-05-08 18:56:33 +0200

    feat: changes in config firebase

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,11 +3,6 @@ importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
 
 var config = {
-  apiKey: "AIzaSyD8O1xPrTZawSz7BbpuvP6-ugPeO_FOmvc",
-  authDomain: "orderby-a08ae.firebaseapp.com",
-  databaseURL: "https://orderby-a08ae.firebaseio.com",
-  projectId: "orderby-a08ae",
-  storageBucket: "orderby-a08ae.appspot.com",
   messagingSenderId: "401884005336"
 };
 
