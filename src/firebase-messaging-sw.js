76a99e7121997f1b9fe1a60ddc231690616e4672
Moises Garcia <moisesgarcia83@gmail.com>
2019-05-08 18:50:11 +0200

    feat:changes in firebase config

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
-importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-messaging.js');
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
 
+var config = {
+  apiKey: "AIzaSyD8O1xPrTZawSz7BbpuvP6-ugPeO_FOmvc",
+  authDomain: "orderby-a08ae.firebaseapp.com",
+  databaseURL: "https://orderby-a08ae.firebaseio.com",
+  projectId: "orderby-a08ae",
+  storageBucket: "orderby-a08ae.appspot.com",
+  messagingSenderId: "401884005336"
+};
 
-
-firebase.initializeApp({messagingSenderId : '401884005336'});
+firebase.initializeApp(config);
 const messaging = firebase.messaging();
-// self.addEventListener('notificationclick', function(event) {
-//   console.log('[Service Worker] Notification click Received.');
-//   event.notification.close();
-//   event.waitUntil(
-//     clients.openWindow('http://localhost:3000/#/messages/')
-//   );
-// });
