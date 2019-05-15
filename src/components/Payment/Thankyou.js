0c39d5e86e7b8c71126be066adfa004409526dca
Moises Garcia <moisesgarcia83@gmail.com>
2019-05-15 11:26:28 +0200

    feat: rate in thankyou page done

--- src/components/Payment/Thankyou.js
+++ src/components/Payment/Thankyou.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react'
 import GoodBye from '../../sources/mano.svg'
 import { withRouter } from 'react-router-dom'
 import './Thankyou.css'
+import WaiterRate from '../WaiterInfo/WaiterRate';
+import '../MenuSide/ActionButton.css'
+import Plus from '../../sources/vectorpaint.svg'
 
 class Thankyou extends Component {
 
   state = {
-    table : {}
+    table : {},
+    callActive : false,
   }
 
  goHome = () =>{
     this.props.history.push('/board') 
   }
+  showMessage = () => {
+    this.setState({
+      callActive: this.state.callActive ? false : true
+    })
+  }
 
   render() {
     return (
       <div>
+        {this.state.callActive && <WaiterRate blur={this.state.callActive} {...this.state.waiter} />}
           <div className=''>
             <div className='d-flex  absolute-payment-arrow container mt-4'>
               <span className="material-icons white" onClick={this.goHome} >arrow_back</span>
@@ -29,9 +39,13 @@ class Thankyou extends Component {
               </div>
                 <h5 className='mt-4 white text-center'>¡Oído cocina!</h5>
                 <h5 className=' white enjoyed'>El pago se ha realizado correctamente</h5>
+                <h5 className=' white enjoyed'>Por favor, valora la atención recibida por nuestro personal pulsando en la estrella</h5>
               </div>
             </div>
           </div> 
+          <div className='container-button-more'>
+        <button className="pure-material-button-contained" onClick={this.showMessage}><img className='more-icon-actionbutton' alt='more' src={Plus}/></button>
+      </div>
       </div>
     );
   }
